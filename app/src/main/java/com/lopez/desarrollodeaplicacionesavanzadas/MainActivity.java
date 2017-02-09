5294347efab557cbccfe37321acf2fa8ba5c0f65
lopez0412 <lopez.javier0412@gmail.com>
2017-02-08 21:22:08 -0600

    Initial commit

---
+++ app/src/main/java/com/lopez/desarrollodeaplicacionesavanzadas/MainActivity.java
@@ -0,0 +1,122 @@
+package com.lopez.desarrollodeaplicacionesavanzadas;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView adView;
+    private CallbackManager cM;
+    private LoginButton lB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adView = (AdView) findViewById(R.id.ad_view);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        adView.loadAd(adRequest);
+
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        cM = CallbackManager.Factory.create();
+
+        getFbKeyHash("3ZLW/TAqPvR43Zh79ejFQDOdka8=");
+
+        setContentView(R.layout.activity_main);
+
+        lB = (LoginButton)findViewById(R.id.login_facebook);
+
+        lB.registerCallback(cM, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión exitoso!", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión cancelado!", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+                Toast.makeText(MainActivity.this, "¡Inicio de sesión NO exitoso!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        if(adView != null){
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if(adView != null){
+            adView.resume();
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(adView != null){
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+    public void getFbKeyHash(String packageName){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    packageName, PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                System.out.println("KeyHash: " + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e){
+
+        }catch (NoSuchAlgorithmException e){
+
+        }
+
+    }
+    protected void onActivityResult(int reqCode, int resCode, Intent i){
+        cM.onActivityResult(reqCode, resCode, i);
+    }
+}
